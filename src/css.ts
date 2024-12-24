0d421483745170fb8d19de81c3425416304e8a32
Oleg Podsechin <oleg.podsechin@toughbyte.com>
2024-12-24 13:38:42 +0200

    moved assets to assets directory

--- src/css.ts
+++ src/css.ts
@@ -4,7 +4,7 @@ import * as path from "path";
 (async function () {
   const url =
     "https://cdn.jsdelivr.net/npm/@tabler/core/dist/css/tabler.min.css";
-  const dir = path.join(process.cwd(), "site", "css");
+  const dir = path.join(process.cwd(), "site", "assets", "css");
   const file = path.join(dir, "tabler.css");
 
   const response = await fetch(url);
